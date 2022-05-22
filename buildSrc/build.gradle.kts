c02596913c3d8b1386fd6da60d7c9aa74ec90d9f
Eugene Petrenko <me@jonnyzzz.com>
2022-05-22 23:01:50 +0200

    it works!

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,4 +4,15 @@ plugins {
 
 repositories {
   mavenCentral()
+  gradlePluginPortal()
 }
+
+dependencies {
+  fun pluginDependency(id: String, version: String) {
+    implementation("$id:$id.gradle.plugin:$version")
+  }
+
+  pluginDependency("org.jetbrains.kotlin.jvm", "1.6.20")
+  pluginDependency("org.jetbrains.intellij", "1.5.2")
+}
+
