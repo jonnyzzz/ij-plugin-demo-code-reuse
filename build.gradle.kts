0558f965c1ae836ccdc97a09d1e13a3ef8a4c174
Eugene Petrenko <me@jonnyzzz.com>
2022-05-22 22:15:01 +0200

    project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.6.20"
+  id("org.jetbrains.intellij") version "1.5.2"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+intellij {
+  version.set("2021.2")
+  type.set("IC") // Target IDE Platform
+  plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks.withType<JavaCompile> {
+  sourceCompatibility = "11"
+  targetCompatibility = "11"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  kotlinOptions.jvmTarget = "11"
+}
